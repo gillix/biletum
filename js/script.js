db958e0382fa8b73cd3dcfa05da41bc6e0b9fcac
Artem Kikhtenko <artemka81@gmail.com>
2021-10-08 08:26:17 +0300

    Fixed text bugs

--- js/script.js
+++ js/script.js
@@ -63,14 +63,14 @@ const langArr = {
 		"ua": "Єдина багатофункціональна платформа для проведення заходів, автоматизації дистрибуції, продажу квитків і експо-стендів."
 	},
 	"decs":{
-		"ru": "Biletum — инновационная экосистема сервисов и платформ для организации бизнес-мероприятий любого масштаба и формата проведения",
-		"en": "Biletum is an innovative ecosystem of services and platforms for organizing business events of any scale and format",
-		"ua": "Biletum — інноваційна екосистема сервісів і платформ для організації бізнес-заходів будь-якого масштабу та формату проведення"
+		"ru": "Biletum — инновационная экосистема сервисов и&nbsp;платформ для организации бизнес-мероприятий любого масштаба и формата проведения.",
+		"en": "Biletum is an innovative ecosystem of services and&nbsp;platforms for organizing business events of any scale and format.",
+		"ua": "Biletum — інноваційна екосистема сервісів і платформ для організації бізнес-заходів будь-якого масштабу та&nbsp;формату проведення."
 	},
 	"menu__list":{
-		"ru": ['Biletium', 'Услуги', 'Партнеры', 'Преимущества', 'Команда','Контакты'],
-		"en": ['Biletium', 'Services', 'Partners', 'Advantages', 'Team', 'Contacts'],
-		"ua": ['Biletium', 'Послуги', 'Партнери', 'Переваги', 'Команда', 'Контакти']
+		"ru": ['Biletum', 'Услуги', 'Партнеры', 'Преимущества', 'Команда','Контакты'],
+		"en": ['Biletum', 'Services', 'Partners', 'Advantages', 'Team', 'Contacts'],
+		"ua": ['Biletum', 'Послуги', 'Партнери', 'Переваги', 'Команда', 'Контакти']
 	},
 	"btn-sale":{
 		"ru": "Хочу продавать билеты",
@@ -108,27 +108,27 @@ const langArr = {
 		"ua": "Дізнатися більше"
   },
   "slider__text1":{
-		"ru": ['Продажа билетов и экспо-стендов', 'автоматизация процесса регистрации и продаж', 'единое диджитал пространство для работы внутреннего отдела продаж и партнеров', 'полный спектр маркетинговых инструментов для увеличения конверсии и продаж'],
-		"en": ['Sale tickets and exhibition stands', 'registration and sales process automation', 'single digital space for the internal sales department and partners work', 'full range of marketing tools to increase conversions and sales'],
-		"ua": ['Продаж квитків та експостендів', 'автоматизація процесу реєстрації та продажів', 'єдиний діджитал-простір для роботи внутрішнього відділу продажів і партнерів', 'повний спектр маркетингових інструментів для збільшення конверсії і продажів']
+		"ru": ['Продажа билетов и&nbsp;экспо-стендов:', 'Автоматизация процесса регистрации и продаж;', 'Единое диджитал пространство для работы внутреннего отдела продаж и партнеров;', 'полный спектр маркетинговых инструментов для увеличения конверсии и продаж.'],
+		"en": ['Sale tickets and exhibition stands:', 'registration and sales process automation', 'single digital space for the internal sales department and partners work', 'full range of marketing tools to increase conversions and sales'],
+		"ua": ['Продаж квитків та&nbsp;експостендів:', 'Автоматизація процесу реєстрації та продажів', 'Єдиний діджитал-простір для роботи внутрішнього відділу продажів і партнерів;', 'Повний спектр маркетингових інструментів для збільшення конверсії і продажів.']
 	},
   "slider__text2":{
-		"ru": ['Дистрибуция билетов и экспо-стендов', ' автоматизация управления продажами агентов и партнеров', 'дистрибуция в сети Biletum - 12 978 партнеров в 86 странах мира', 'привлечение покупателей из аудитории Biletum - более 10 млн. пользователей'],
-		"en": ['Distribute tickets and exhibition stands', 'automation of sales management of agents and partners', 'distribution in the Biletum network - 12,978 partners in 86 countries of the world', 'attracting buyers from the Biletum audience - more than 10 million users'],
-		"ua": ['Дистрибуція квитків та експостендів', 'автоматизація управління продажами агентів та партнерів', 'дистрибуція в мережі Biletum - 12 978 партнерів у 86 країнах світу', 'залучення покупців з аудиторії Biletum – понад 10 млн користувачів']
+		"ru": ['Дистрибуция билетов и экспо-стендов:', 'Автоматизация управления продажами агентов и партнеров;', 'Дистрибуция в сети Biletum — 12 978 партнеров в 86 странах мира;', 'Привлечение покупателей из аудитории Biletum — более 10 млн пользователей.'],
+		"en": ['Distribute tickets and exhibition stands:', 'Automation of sales management of agents and partners;', 'Distribution in the Biletum network — 12,978 partners in 86 countries of the world;', 'Attracting buyers from the Biletum audience — more than 10 million users.'],
+		"ua": ['Дистрибуція квитків та&nbsp;експостендів:', 'Автоматизація управління продажами агентів та партнерів;', 'Дистрибуція в мережі Biletum - 12 978 партнерів у 86 країнах світу;', 'Залучення покупців з аудиторії Biletum – понад 10 млн користувачів.']
 	},
   "slider__text3":{
-		"ru": ['Платформы для проведения мероприятий', ' интеграция с 5 платформами для любого формата проведения мероприятий', 'полная автоматизация передачи данных с платформами', 'автоматическое создание мобильного приложения мероприятия'],
-		"en": ['Event Execution Platforms', 'integration with 5 platforms for events of any format', 'full automation of data transfer', 'automatic generation of mobile app for the event'],
-		"ua": ['Платформи для проведення заходів', ' інтеграція з 5 платформами для будь-якого формату проведення заходів', 'повна автоматизація передавання даних з платформами', 'автоматичне створення мобільного застосунку заходу']
+		"ru": ['Платформы для&nbsp;проведения мероприятий:', 'Интеграция с 5 платформами для любого формата проведения мероприятий;', 'Полная автоматизация передачи данных с платформами;', 'Автоматическое создание мобильного приложения мероприятия.'],
+		"en": ['Event Execution Platforms:', 'Integration with 5 platforms for events of any format;', 'Full automation of data transfer;', 'Automatic generation of mobile app for the event.'],
+		"ua": ['Платформи для проведення заходів:', ' Інтеграція з 5 платформами для будь-якого формату проведення заходів;', 'Повна автоматизація передавання даних з платформами;', 'Автоматичне створення мобільного додатку заходу.']
 	},
   "skills__list":{
-		"ru": ['Топ-3 платформа по продаже билетов в Восточной Европе;', 'Лучший стартап года по версии The Next Web;', '#6 в Топ-50 стартапов года согласно рейтингу «Коммерсантъ»;', 'Гран-при ежегодной премии «Облака».'],
-		"en": ['Top 3 platforms for ticket sales in Eastern Europe;', 'Best Startup of the Year by The Next Web;', '#6 in the Top 50 startups of the year according to the Kommersant rating;', 'Grand Prix of the annual Cloud Awards.'],
-		"ua": ['Топ-3 платформа з продажу квитків у Східній Європі;', 'Кращий стартап року за версією The Next Web;', '#6 в Топ-50 стартапів року згідно з рейтингом «Коммерсантъ»;', 'Гран-прі щорічної премії «Хмари».']
+		"ru": ['Топ-3 платформа по продаже билетов в&nbsp;Восточной Европе;', 'Лучший стартап года по версии The&nbsp;Next&nbsp;Web;', '#6 в Топ-50 стартапов года согласно рейтингу «Коммерсантъ»;', 'Гран-при ежегодной премии «Облака».'],
+		"en": ['Top 3 platforms for ticket sales in&nbsp;Eastern Europe;', 'Best Startup of the Year by The&nbsp;Next&nbsp;Web;', '#6 in the Top 50 startups of the year according to the Kommersant rating;', 'Grand Prix of the annual Cloud Awards.'],
+		"ua": ['Топ-3 платформа з продажу квитків у&nbsp;Східній Європі;', 'Кращий стартап року за версією The&nbsp;Next&nbsp;Web;', '#6 в Топ-50 стартапів року згідно з&nbsp;рейтингом «Коммерсантъ»;', 'Гран-прі щорічної премії «Хмари».']
 	},
   "skills__list2":{
-		"ru": ['Топ-10 зарубежных IT-компаний, которые успешно вышли на китайский рынок по версии RISE;', 'Крупнейшая платформа для дистрибуции билетов и стендов на выставки Китая', 'Эксклюзивный дистрибьютор на крупнейшие мероприятия, включая Шанхайский автосалон;', 'Внеконкурсный участник программы Chinaccelerator (SOSV).'],
+		"ru": ['Топ-10 зарубежных IT-компаний, которые успешно вышли на китайский рынок по&nbsp;версии RISE;', 'Крупнейшая платформа для дистрибуции билетов и стендов на выставки Китая;', 'Эксклюзивный дистрибьютор на крупнейшие мероприятия, включая Шанхайский автосалон;', 'Внеконкурсный участник программы Chinaccelerator (SOSV).'],
 		"en": ['Top 10 foreign IT companies to have successfully entered the Chinese market according to RISE;', 'The largest platform for distribution of tickets and exhibition stands in China;', 'Exclusive distributor for major events including the Shanghai Auto Show;', 'Out-of-Competition Participant of Chinaccelerator (SOSV) program.'],
 		"ua": ['Топ-10 зарубіжних IT-компаній, які успішно вийшли на китайський ринок за версією RISE;', 'Найбільша платформа для дистрибуції квитків і стендів на виставки Китаю;', "Ексклюзивний дистриб'ютор на найбільші заходи, зокрема Шанхайський автосалон;", 'Позаконкурсний учасник програми Chinaccelerator (SOSV).']
 	},
@@ -148,9 +148,9 @@ const langArr = {
 		"ua": "пропозиції?"
   },
   "card__desc1":{
-    "ru": "Обеспечиваем реальный рост продаж билетов и экспо-стендов, от 27% и выше",
+    "ru": "Обеспечиваем реальный рост продаж билетов и&nbsp;экспо-стендов, от&nbsp;27% и выше",
 		"en": "We provide significant growth in ticket sales and exhibition stands, from 27% and more",
-		"ua": "Забезпечуємо реальне зростання продажів квитків та експостендів, від 27% і вище"
+		"ua": "Забезпечуємо реальне зростання продажів квитків та експостендів — від 27&nbsp;% і вище"
   },
   "card__desc2":{
     "ru": "Обеспечиваем мультиязычную поддержку покупателей",
@@ -160,22 +160,22 @@ const langArr = {
   "card__desc3":{
     "ru": "Получаем только фиксированную абонплату за сервис от $ 49 в месяц",
 		"en": "We receive a fixed monthly fee for the service from $ 49 per month",
-		"ua": "Отримуємо тільки фіксовану абонплату за сервіс від $49 в місяць"
+		"ua": "Отримуємо тільки фіксовану абонплату за сервіс — від $ 49 в місяць"
   },
   "card__desc4":{
-    "ru": "Обеспечиваем прием оплат за билеты и экспо-стенды сразу на счет организатора",
+    "ru": "Обеспечиваем прием оплат за билеты и&nbsp;экспо-стенды сразу на счет организатора",
 		"en": "We ensure that all payments for tickets and exhibition stands are transferred to the account of the organizer immediately",
-		"ua": "Забезпечуємо прийом оплат за квитки й експостенди відразу на рахунок організатора"
+		"ua": "Забезпечуємо прийом оплат за квитки й&nbsp;експостенди відразу на рахунок організатора"
   },
   "card__desc5":{
-    "ru": "Охватываем самый большой рынок по гео охвату и емкости",
+    "ru": "Охватываем самый большой рынок по&nbsp;гео&nbsp;охвату и емкости",
 		"en": "We cover the largest market in terms of geo-coverage and capacity",
-		"ua": "Охоплюємо найбільший ринок за геоосяжністю та місткості"
+		"ua": "Охоплюємо найбільший ринок за&nbsp;геоосяжністю та місткості"
   },
   "card__desc6":{
     "ru": "Предоставляем единый набор инструментов для работы организатора в одном окне ",
 		"en": "We provide a single set of tools for the organizer's work in one window",
-		"ua": "Надаємо єдиний набір інструментів для роботи організатора в одному вікні"
+		"ua": "Надаємо єдиний набір інструментів для&nbsp;роботи організатора в одному вікні"
   },
   "card__span1":{
     "ru": "от",
@@ -195,7 +195,7 @@ const langArr = {
   "clients":{
 		"ru": ['Клиенты', 'Нашими услугами уже пользуются крупнейшие организаторы мероприятий Китая, России и Восточной Европы, включая Canton Fair, UBM, VNU, HKTDC, SIEC. Общий оборот текущих клиентов составляет более $2 млрд. в год.',],
 		"en": ['Clients', 'Our services are already used by the largest event organizers in China, Russia and Eastern Europe, including Canton Fair, UBM, VNU, HKTDC, SIEC. The total turnover of current clients is more than $ 2 billion per year.'],
-		"ua": ['Клієнти', 'Нашими послугами вже користуються найбільші організатори заходів Китаю, Росії та Східної Європи, зокрема Canton Fair, UBM, VNU, HKTDC, SIEC. Загальний обіг поточних клієнтів становить понад $2 млрд на рік.',]
+		"ua": ['Клієнти', 'Нашими послугами вже користуються найбільші організатори заходів Китаю, Росії та Східної Європи, зокрема Canton Fair, UBM, VNU, HKTDC, SIEC. Загальний обіг поточних клієнтів становить понад $2 млрд. на рік.',]
 	},
   "partners":{
 		"ru": ['Партнеры', 'Привлечь ещё больше посетителей на мероприятия поможет сеть партнёров Biletum.',],
@@ -265,7 +265,7 @@ const langArr = {
   "team":{
     "ru": ['Команда', 'В нашей команде более 50 экспертов, каждый из которых отвечает за результат вашей работы с Biletum.',],
 		"en": ['Team', 'Our team includes over 50 experts, each of whom is responsible for the outcome of your work with Biletum.'],
-		"ua": ['Команда', 'У нашій команді понад 50 експертів, кожен з яких є відповідальним за результат вашої роботи з Biletum.',]
+		"ua": ['Команда', 'У нашій команді понад 50 експертів, кожен з яких є&nbsp;відповідальним за результат вашої роботи з Biletum.',]
   },
   "team-card1":{
     "ru": ['Геннадий Нетяга', 'Chief Executive Officer (Europe, Asia)'],
@@ -305,27 +305,27 @@ const langArr = {
   "team-card8":{
     "ru": ['Эмма Джанг', 'Business Development Manager (Asia)'],
 		"en": ['Emma Zhang', 'Business Development Manager (Asia)'],
-		"ua": ['Емма Джанг', 'Business Development Manager (Asia)']
+		"ua": ['Емма Джанґ', 'Business Development Manager (Asia)']
   },
   "footer__item1":{
-    "ru": ['Украина,', 'Киев,<br>ул. Казимира Малевича 86-Л, 2-й этаж'],
-		"en": ['Ukraine,', 'Kyiv,<br>86L Kazymyra Malevicha St., 2nd floor'],
-		"ua": ['Україна,', 'Київ,<br>вул. Казимира Малевича, 86Л, ​​2-й поверх']
+    "ru": ['Украина', 'Киев,<br>ул. Казимира Малевича, 86-Л, 2-й этаж'],
+		"en": ['Ukraine', 'Kyiv,<br>86L Kazymyra Malevicha, St., 2nd floor'],
+		"ua": ['Україна', 'Київ,<br>вул. Казимира Малевича, 86Л, ​​2-й поверх']
   },
   "footer__item2":{
-    "ru": ['Россия,', 'Москва,<br>ул. Летниковская 10, строение 1, 7-й этаж'],
-		"en": ['Russia,', 'Moscow,<br>10 Letnikovskaya St., building 1, 7th floor'],
-		"ua": ['Росія,', 'Москва,<br>вул. Летніковська, 10, корпус 1, 7-й поверх']
+    "ru": ['Россия', 'Москва,<br>ул. Летниковская, 10, строение 1, 7-й этаж'],
+		"en": ['Russia', 'Moscow,<br>10 Letnikovskaya, St., building 1, 7th floor'],
+		"ua": ['Росія', 'Москва,<br>вул. Летніковська, 10, корпус 1, 7-й поверх']
   },
   "footer__item3":{
-    "ru": ['Китай,', 'Шанхай,<br>р-н. 2115, 21F, East Ocean Centre Phase, № 618 East Yan An Road'],
-		"en": ['China,', 'Shanghai,<br>21F East Ocean Centre Phase, No.618 East Yan An Road'],
-		"ua": ['Китай,', 'Шанхай,<br>обл. 2115, 21F, East Ocean Ctntre Phase, №.618 East Yan An Road']
+    "ru": ['Китай', 'Шанхай,<br>р-н. 2115, 21F, East Ocean Centre Phase, № 618 East Yan An Road'],
+		"en": ['China', 'Shanghai,<br>21F East Ocean Centre Phase, No.618 East Yan An Road'],
+		"ua": ['Китай', 'Шанхай,<br>обл. 2115, 21F, East Ocean Centre Phase, №.618 East Yan An Road']
   },
   "footer__item4":{
-    "ru": ['Великобритания,', 'Лондон,<br>ул. Джона Принца 13, 2-й этаж'],
-		"en": ['United Kingdom,', 'London,<br>13 John Prince’s St., 2nd floor'],
-		"ua": ['Великобританія,', 'Лондон,<br>вул. Джона Принца 13, Лондон W1G 0J, 2-й поверх']
+    "ru": ['Великобритания,', 'Лондон,<br>ул. Джона Принца, 13, 2-й&nbsp;этаж'],
+		"en": ['United Kingdom,', 'London,<br>13 John Prince’s St., 2nd&nbsp;floor'],
+		"ua": ['Великобританія,', 'Лондон,<br>вул. Джона Принца, 13, Лондон W1G 0J, 2-й&nbsp;поверх']
   },
   "cta__wrapper":{
     "ru": ['Увеличьте конверсию в <span>10 раз</span>', 'с минимальными затратами ресурсов'],
